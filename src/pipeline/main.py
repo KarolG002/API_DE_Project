a7e6b23d9d17fa1c6143887e2d426d9909a0346b
DESKTOP-197DBPJ <karolgawlakanalityka@gmail.com>
2024-08-27 12:54:32 +0200

    Created fake api and downloaded data

---
+++ src/pipeline/main.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "http://127.0.0.1:8000/v2/random-data"
+def get_api_data() -> dict:
+    try:
+        response = requests.get(url)
+        random_data_dict = response.json()
+        return random_data_dict
+    except requests.exceptions.RequestException as e:
+        print("Could not access API")
+        raise SystemExit(e)
+    
+def main():
+    random_data = get_api_data()
+    print(random_data)
+
+if __name__ == "__main__":
+    main()
